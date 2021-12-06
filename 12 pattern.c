c353c817aa592b2e522efbfe83f1c14a47c3a292
vaibhav-singh1 <93764777+vaibhav-singh1@users.noreply.github.com>
2021-12-06 23:37:31 +0530

    Add files via upload

---
+++ 12 pattern.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+   printf("GIVE THE NBR");
+    scanf("%d",&n);
+   { for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++)
+        printf("%d",j);
+        
+        
+       printf("\n"); }}
+       
+    
+    
+
+
+    return 0;
+ 
+}
+
